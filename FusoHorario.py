d5bdcfedbe2e89b04747863619ead5516749850b
joaomalfatti <joao.cursos1210@gmail.com>
2025-07-15 19:47:32 -0400

    Melhorando a lógica e ajustes

--- FusoHorario.py
+++ FusoHorario.py
@@ -1,50 +1,48 @@
 import os
 
-def exibir_menu():
+def mostrar_menu():
     os.system("cls")
     print("\n" + "="*50)
-    print("=== Alterar Fuso Horário - v2.0 ===")
+    print("=== Fuso Horário - v3.0 ===")
     print("="*50)
-    print("\n--- Alterar Fuso Horário no Windows ---")
-    print(" 1. (UTC-04:00) Manaus")
-    print(" 2. (UTC-05:00) Rio Branco")
-    return input("\n Escolha um fuso (1-2) ou pressione Enter para sair: ")
+    print("\n Menu de Fusos Horários")
+    print("="*30)
+    print("1. Manaus (UTC-04:00)")
+    print("2. Rio Branco (UTC-05:00)")
+    print("\n")
 
 def alterar_fuso(opcao):
     fusos = {
-        "1": {
-            "nome": "SA Western Standard Time",
-            "cidade": "Manaus",
-        },
-        "2": {
-            "nome": "SA Pacific Standard Time",
-            "cidade": "Rio Branco",
-        }         
+        "1": {"nome": "SA Western Standard Time", "cidade": "Manaus"},
+        "2": {"nome": "SA Pacific Standard Time", "cidade": "Rio Branco"}
     }
-
-    if opcao not in fusos:
-        print("Opção inválida!")
-        return False
     
-    try:
+    if opcao in fusos:
         os.system(f'tzutil /s "{fusos[opcao]["nome"]}"')
-        print(f" Fuso alterado para: {fusos[opcao]["cidade"]}")
+        print(f"\nPronto! Fuso horário alterado para {fusos[opcao]["cidade"]}.\n")
         return True
-    except Exception as error:
-         print(f" Erro alterar fuso: {error}")
-         return False
-
+    
+    print("\nOpção não reconhecida. Por favor, tente novamente.\n")
+    return False
 
 def main():
-    os.system("cls")
-    opcao = exibir_menu()
-    if opcao:  
-        alterar_fuso(opcao)
-    print(" Saindo...")
+    while True:
+        mostrar_menu()
+        escolha = input("Escolha o fuso horário (1-2) ou 'S' para sair: ").upper()
+            
+        if alterar_fuso(escolha):
+            continuar = input("Deseja alterar outro fuso? (S/N): ").upper()
+            if continuar != 'S':
+                print("\n" + "="*50)
+                print("=== Encerrando o programa ===")
+                print("="*50)
+                break
 
 if __name__ == "__main__":
     main()
 
 
-""" Version: 2.0.0
-Creator: João Malfatti """
+'''
+Version: 3.0.0
+Creator: João Malfatti
+'''
